2114232b89ad8a068e3a3be50d987617c1f97311
Matthew Sousa <mattsousatwo@gmail.com>
2021-06-06 01:39:51 -0400

    Initial Commit

---
+++ OnTack/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  OnTack
+//
+//  Created by Matthew Sousa on 6/6/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
